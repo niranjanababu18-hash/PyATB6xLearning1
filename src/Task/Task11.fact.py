891f864c97dea07bf026bcdd70196e901f2fa187
neenu <niranjanababu18@gmail.com>
2025-10-29 08:58:03 +0530

    Task-18/10/2025

--- src/Task/Task11.fact.py
+++ src/Task/Task11.fact.py
@@ -1,7 +1,10 @@
 #factorial
-#Question 1. :Given a Number a number you need to calculate the factorial of that number
+#Question 1:Given a  number you need to calculate the factorial of that number
 num=int(input("enter number"))
 fact=1
-for i in range(1,num+1):
-    fact=fact*i
+if num<=0:
+    print("fact is ", fact)
+else:
+    for i in range(1,num+1):
+        fact=fact*i
 print("factorial is: ",fact)
